bee32704adee7f117b0fad39652a4499ac0a707a
SummerToday <kom347@naver.com>
2024-01-30 20:33:03 +0900

    Initial commit

---
+++ demo/src/main/java/com/example/demo/InKyung/MemberRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.InKyung;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
